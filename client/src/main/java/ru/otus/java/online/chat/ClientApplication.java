3f9c4ecc2d9628134479eae2f934df34b87d2368
Andrey Melnik <andreum.melnik@yandex.ru>
2024-07-06 17:26:02 +0300

    commit

--- client/src/main/java/ru/otus/java/online/chat/ClientApplication.java
+++ client/src/main/java/ru/otus/java/online/chat/ClientApplication.java
@@ -1,9 +1,1 @@
-package ru.otus.java.online.chat;
 
-import java.io.IOException;
-
-public class ClientApplication {
-    public static void main(String[] args) throws IOException {
-        new Client();
-    }
-}
